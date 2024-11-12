864bad9d2ab345cef75916c58dbb8368795898a8
fhfld <seehb126@naver.com>
2024-11-12 18:46:20 +0900

    프로젝트 초기 세팅

---
+++ src/main/java/study/withkbo/WithKboApplication.java
@@ -0,0 +1,13 @@
+package study.withkbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithKboApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WithKboApplication.class, args);
+    }
+
+}
