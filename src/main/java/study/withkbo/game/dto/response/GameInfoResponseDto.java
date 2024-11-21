7676421d2e5d3a0691a32a7c24b5ca1fcb05b458
fhfld <seehb126@naver.com>
2024-11-21 16:54:16 +0900

    ✨feat: s3Bucket 설정 추가

--- src/main/java/study/withkbo/game/dto/response/GameInfoResponseDto.java
+++ src/main/java/study/withkbo/game/dto/response/GameInfoResponseDto.java
@@ -15,8 +15,10 @@ public class GameInfoResponseDto {
     private String matchTime;
     private String homeTeam;
     private String homeTeamScore;
+    private String homeTeamLogoImg;
     private String awayTeam;
     private String awayTeamScore;
+    private String awayTeamLogoImg;
     private String stadium;
     private String gameSort;
     private String tv;
